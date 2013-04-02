fb6714f6f9e69b789ffeb71da5773b5abee1f8e6
odesey <odesey@gmail.com>
2013-04-01 21:55:58 -0400

    staring over, worked once and then quit

--- movies.rb
+++ movies.rb
@@ -1,14 +1,33 @@
+# require 'test/unit'
+require 'sqlite3'
+require 'httparty'
+require 'json'
+require 'pry'
+
+
+
 class Movie
+    attr_accessor :title
+    attr_accessor :year
+    attr_accessor :rating
+    attr_accessor :genre
+    attr_accessor :director
+    attr_accessor :actors
+    attr_accessor :plot
+    
 
   # Add attr_accessors for the values you want to store...
 
-  def self.get_film_info(name)    
+  def self.get_film_info(name)
+    binding.pry
+    name = gets.chomp
+    puts "Adding #{name} to the database."    
     imdb_data = HTTParty.get("http://www.omdbapi.com/?t=#{name}")
     movie_info = JSON(imdb_data)
     binding.pry
 
     # Create a Movie object...
-    m = Movie.new
+    # m = Movie.new
 
     # Fill in the attributes...
 
@@ -16,3 +35,5 @@ class Movie
   end
 
 end
+
+Movie.new
