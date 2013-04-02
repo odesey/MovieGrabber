756254b5c82ed3d38327d2127348e4ad31c510e3
odesey <odesey@gmail.com>
2013-04-01 23:44:41 -0400

    created DB and all the needed tables, even used foreign keys

--- movies.rb
+++ movies.rb
@@ -19,16 +19,32 @@ class Movie
   # Add attr_accessors for the values you want to store...
 
   def self.get_film_info(name)
-    binding.pry
-    # name = gets.chomp
     puts 'Adding "#{name}" to the database.'
     imdb_data = HTTParty.get("http://www.omdbapi.com/?t=#{name}")
     movie_info = JSON(imdb_data)
     binding.pry
 
     # Create a Movie object...
+    db = SQLite3::Database.new("movies.db")
     new_movie = Movie.new
 
+def self.all
+    people = []
+    # binding.pry
+    results = @@db.execute("select * from people")
+    # binding.pry
+
+    results.each do |i| row
+      newguy = Person.create_person(i[1])
+      newguy.id = i[0]
+      newguy.type = i[1]
+      newguy.name = i[2]
+      newguy.email = i[3]
+binding.pry
+      newguy << people
+      
+
+      sql = "insert into people (name, age, height, star_sign, married) values (?, ?, ?, ?, ?)"
     # Fill in the attributes...
 
     # Store me in a SQLite3 database...    
